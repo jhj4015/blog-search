9a265af729063cc3a4a62ee43b1a67042c854eaa
hyunjeong.jeong <i.m.ready0366@gmail.com>
2022-09-20 18:41:14 +0900

    feat : TrendKeyword Test 코드 작성

--- src/test/java/com/jhj/blogsearch/BlogSearchApplicationTests.java
+++ src/test/java/com/jhj/blogsearch/BlogSearchApplicationTests.java
@@ -2,7 +2,9 @@ package com.jhj.blogsearch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootTest
 class BlogSearchApplicationTests {
 
